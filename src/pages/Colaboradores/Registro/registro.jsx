c3e66f5233cf82e6607ae409fb7665fd1641d764
ariany-f <ariany_f@hotmail.com>
2023-11-14 10:42:06 -0300

    remoção erros e tela de registro colaborador inicio

--- src/pages/Colaboradores/Registro/registro.jsx
+++ src/pages/Colaboradores/Registro/registro.jsx
@@ -1,7 +1,51 @@
+import CampoTexto from "@components/CampoTexto"
+import { useState } from "react"
+
 function ColaboradorRegistro() {
-    const url = window.location.pathname;
+
+    const [classError, setClassError] = useState([])
+    const [document, setDocument] = useState('')
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [celular, setCelular] = useState('')
+
     return (
-       <>{url}</>
+        <>
+            <CampoTexto 
+                camposVazios={classError} 
+                patternMask={['999.999.999-99']} 
+                name="document" 
+                valor={document} 
+                setValor={setDocument} 
+                type="text" 
+                label="CPF" 
+                placeholder="Digite o CPF do colaborador" />
+            <CampoTexto 
+                camposVazios={classError} 
+                name="nome" 
+                valor={nome} 
+                setValor={setNome} 
+                type="text" 
+                label="Nome do colaborador" 
+                placeholder="Digite o nome completo do colaborador" />
+            <CampoTexto 
+                camposVazios={classError} 
+                name="email" 
+                valor={email} 
+                setValor={setEmail} 
+                type="email" 
+                label="Email do colaborador" 
+                placeholder="Digite o email do colaborador" />
+            <CampoTexto 
+                camposVazios={classError} 
+                patternMask={['99 9999 9999']} 
+                name="celular" 
+                valor={celular} 
+                setValor={setCelular} 
+                type="text" 
+                label="Celular do colaborador" 
+                placeholder="Digite o telefone do colaborador" />
+        </>
     )
 }
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -22,7 +22,6 @@ function Login() {
         setEmail,
         setCompanies,
         setPassword,
-        setCompanyPublicId,
         solicitarCodigo
     } = useSessaoUsuarioContext()
     
