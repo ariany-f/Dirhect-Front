116dad15a7817d8c121a7ce2843d299f8c4f9673
ariany-f <ariany_f@hotmail.com>
2023-11-24 13:57:41 -0300

    Carteiras

--- src/pages/Colaboradores/Detalhes/saldo.jsx
+++ src/pages/Colaboradores/Detalhes/saldo.jsx
@@ -1,6 +1,28 @@
+import Titulo from '@components/Titulo'
+import { AiFillQuestionCircle } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const QuestionCard = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-weight: 500;
+    & .question-icon {
+        cursor: pointer;
+    }
+`
+
 function ColaboradorSaldo() {
     return (
-        <>Saldo</>
+        <>
+            <Titulo>
+                <QuestionCard>
+                    <h6>Benefícios</h6>
+                    <AiFillQuestionCircle className="question-icon" size={20} />
+                </QuestionCard>
+            </Titulo>
+        </>
     )
 }
 
