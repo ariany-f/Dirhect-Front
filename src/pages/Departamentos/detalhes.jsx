964a822b2f3a58b06e68955ff86e677b5569f3af
ariany-f <ariany_f@hotmail.com>
2023-11-28 01:39:32 -0300

    Titulo departamento

--- src/pages/Departamentos/detalhes.jsx
+++ src/pages/Departamentos/detalhes.jsx
@@ -3,15 +3,19 @@ import { useEffect } from "react";
 import { useParams } from 'react-router-dom';
 import BotaoVoltar from "@components/BotaoVoltar"
 import Frame from "@components/Frame"
+import Texto from "@components/Texto"
+import Titulo from "@components/Titulo"
+import { useState } from 'react';
 
 function DepartamentoDetalhes() {
 
     let { id } = useParams()
+    const [departamento, setDepartamento] = useState({})
 
     useEffect(() => {
         http.get("api/dashboard/department/"+id)
             .then(response => {
-                console.log(response)
+                setDepartamento(response.department)
             })
             .catch(erro => console.log(erro))
     }, [])
@@ -19,6 +23,10 @@ function DepartamentoDetalhes() {
     return (
         <Frame>
             <BotaoVoltar />
+            <Texto weight={500} size="12px">Departamento</Texto>
+            <Titulo>
+                <h3>{departamento.name}</h3>
+            </Titulo>
         </Frame>
     )
 }
