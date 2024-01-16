3ab44bcd6723e364f94283c233a20b58c543be51
ariany-f <ariany_f@hotmail.com>
2024-01-16 14:30:15 -0300

    Seleção forma de pagamento beneficios

--- src/pages/Beneficios/selecao-forma-pagamento.jsx
+++ src/pages/Beneficios/selecao-forma-pagamento.jsx
@@ -2,14 +2,59 @@ import Titulo from '@components/Titulo'
 import SubTitulo from '@components/SubTitulo'
 import ContainerHorizontal from '@components/ContainerHorizontal'
 import CheckBoxContainer from '@components/CheckBoxContainer'
+import Botao from '@components/Botao'
+import Texto from '@components/Texto'
 import Frame from '@components/Frame'
 import CardText from '@components/CardText'
 import styles from './Beneficios.module.css'
 import { useState } from 'react'
+import styled from 'styled-components'
+import { Link, useNavigate } from 'react-router-dom'
+import RadioButton from '../../components/RadioButton'
+
+const CardLine = styled.div`
+    padding: 16px 6px;
+    border-bottom: 1px solid var(--neutro-200);
+    width: 100%;
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    justify-content: start;
+    &:nth-child(1) {
+        padding-top: 8px;
+    }
+    &:last-of-type {
+        border-bottom: none;
+        padding-bottom: 8px;
+    }
+`
+
+const ContainerButton = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    & button {
+        width: initial;
+    }
+`
 
 function BeneficioSelecionarFormaPagamento() {
     
     const [saldoConta, setSaldoConta] = useState(false)
+    const [selectedPaymentOption, setSelectedPaymentOption] = useState(1)
+    const [selectedDate, setSelectedDate] = useState(1)
+    const navegar = useNavigate()
+    
+    function handleChange(valor)
+    {
+        setSelectedPaymentOption(valor)
+    }
+    
+    function handleDateChange(valor)
+    {
+        setSelectedDate(valor)
+    }
+
 
     return (
         <Frame padding="24px 0px">
@@ -22,12 +67,67 @@ function BeneficioSelecionarFormaPagamento() {
             </Titulo>
             <div className={styles.card_dashboard}>
                 <ContainerHorizontal width="50%" align="start" padding="16px">
-                    <Frame gap="5px">
+                    <Frame gap="24px">
                         <CheckBoxContainer fontSize="16px" name="saldo" valor={saldoConta} setValor={setSaldoConta} label="Saldo da Conta"/>
-                        <span>Você pode usar seus créditos em conjunto com <b>Pix, Boleto ou cartão de crédito</b>.</span>
+                        <Texto>Você pode usar seus créditos em conjunto com <b>Pix, Boleto ou cartão de crédito</b>.</Texto>
+                    </Frame>
+                    
+                    <Frame gap="24px">
+                        <CardLine>
+                            <RadioButton top="0" value={1} checked={selectedPaymentOption === 1} onSelected={() => handleChange(1)}/>
+                            <Link>
+                                <Texto aoClicar={() => handleChange(1)} size="16px" weight={700}>Pix</Texto>
+                            </Link>
+                        </CardLine>
+                        
+                        <CardLine>
+                            <RadioButton top="0" value={2} checked={selectedPaymentOption === 2} onSelected={() => handleChange(2)}/>
+                            <Link>
+                                <Texto aoClicar={() => handleChange(2)} size="16px" weight={700}>Boleto Bancário</Texto>
+                            </Link>
+                        </CardLine>
+                        
+                        <CardLine>
+                            <RadioButton top="0" value={3} checked={selectedPaymentOption === 3} onSelected={() => handleChange(2)}/>
+                            <Link>
+                                <Texto aoClicar={() => handleChange(3)} size="16px" weight={700}>Cartão de Crédito</Texto>
+                            </Link>
+                        </CardLine>
+                    </Frame>
+                    <CardText gap="8px" padding="16px">
+                        <p className={styles.subtitulo}>O tempo para confirmação de pagamento para Pix e Cartão de crédito é de até 1 hora. Transferências realizadas por boleto demoram em média 3 dias úteis.</p>
+                    </CardText>
+                </ContainerHorizontal>
+            </div>
+            <Titulo>
+                <h6>Data de depósito dos benefícios</h6>
+                <SubTitulo>Escolha uma data para disponibilizar os benefícios ao seus colaboradores</SubTitulo>
+            </Titulo>
+            
+            <div className={styles.card_dashboard}>
+                <ContainerHorizontal width="50%" align="start" padding="16px">
+                    <Frame gap="24px">
+                        <CardLine>
+                            <RadioButton top="0" value={1} checked={selectedDate === 1} onSelected={() => handleDateChange(1)}/>
+                            <Link>
+                                <Texto aoClicar={() => handleDateChange(1)} size="16px" weight={700}>Pagar hoje</Texto>
+                            </Link>
+                        </CardLine>
+                        
+                        <CardLine>
+                            <RadioButton top="0" value={2} checked={selectedDate === 2} onSelected={() => handleDateChange(2)}/>
+                            <Link>
+                                <Texto aoClicar={() => handleDateChange(2)} size="16px" weight={700}>Agendar pagamento</Texto>
+                            </Link>
+                        </CardLine>
                     </Frame>
                 </ContainerHorizontal>
             </div>
+            
+            <ContainerButton>
+                <Botao aoClicar={() => navegar(-1)} estilo="neutro" formMethod="dialog" size="medium" filled>Cancelar</Botao>
+                <Botao aoClicar={(evento) => {}} estilo="vermilion" size="medium" filled>Confirmar pagamento</Botao>
+            </ContainerButton>
         </Frame>
     )
 }
