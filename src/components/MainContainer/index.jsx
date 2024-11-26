2a31808dccfd2360ece04fffee9a1eb852a094c0
ariany-f <ariany_f@hotmail.com>
2024-11-26 11:34:54 -0300

    Ajuste css central (vai precisar de ajuste)

--- src/components/MainContainer/index.jsx
+++ src/components/MainContainer/index.jsx
@@ -2,15 +2,22 @@ import styled from 'styled-components'
 import styles from './MainContainer.module.css'
 
 const DivContainer = styled.div`
-    align-self: auto;
-    flex-shrink: 1;
-    flex-grow: 1;
-    flex-basis: auto;
-    margin: 0 auto;
-    flex-wrap: initial;
-    justify-content: center;
-    align-items: stretch;
-    max-width: min(100%, 428px);
+    // align-self: auto;
+    // flex-shrink: 1;
+    // flex-grow: 1;
+    // flex-basis: auto;
+    // margin: 0 auto;
+    // flex-wrap: initial;
+    // justify-content: center;
+    // align-items: stretch;
+    // max-width: min(100%, 428px);
+
+     justify-content: ${ props => props.$align ? props.$align : 'center' };
+    align-items: ${ props => props.$align == 'center' ? props.$align : 'initial' };
+    text-align: ${ props => props.$align ? props.$align : 'center' };
+    padding: ${ props => props.$padding ? props.$padding : '5vw 10vw' };
+    overflow-y: auto;
+    width: -webkit-fill-available;
 `
 
 function MainContainer({ children, align, padding = '0 10vw', aoClicar = null }) {
