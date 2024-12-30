2f818d5021c8352e64ded9c086a5e0e7a0e2e03b
ariany-f <ariany_f@hotmail.com>
2024-12-30 17:42:16 -0300

    Remover links bottom

--- src/components/RodapePublico/index.jsx
+++ src/components/RodapePublico/index.jsx
@@ -5,9 +5,9 @@ import logotransparent from './logo-transparent.png'
 function RodapePublico() {
     return (
         <footer className={styles.footer}>
-            <img src={logotransparent}></img>
-            <Link className={styles.link}>Termos de uso</Link>
-            <Link className={styles.link}>Política de privacidade</Link>
+            {/* <img src={logotransparent}></img> */}
+            {/* <Link className={styles.link}>Termos de uso</Link> */}
+            {/* <Link className={styles.link}>Política de privacidade</Link> */}
         </footer>
     )
 }
