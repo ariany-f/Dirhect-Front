0de60b92c10ba8cc7aeb06111266080445c3dbe9
ariany-f <ariany_f@hotmail.com>
2025-02-04 20:03:21 -0300

    Formulario de Férias

--- src/pages/Ausencias/Registro/dados-iniciais.jsx
+++ src/pages/Ausencias/Registro/dados-iniciais.jsx
@@ -4,11 +4,7 @@ import Frame from "@components/Frame"
 import Titulo from "@components/Titulo"
 import Texto from "@components/Texto"
 import QuestionCard from '@components/QuestionCard'
-import SwitchInput from '@components/SwitchInput'
-import DropdownItens from '@components/DropdownItens'
-import CheckboxContainer from "@components/CheckboxContainer"
 import Loading from "@components/Loading"
-import DepartamentosRecentes from "@components/DepartamentosRecentes"
 import { useState, useRef, useEffect } from "react"
 import http from '@http'
 import styles from './Registro.module.css'
@@ -17,9 +13,7 @@ import { RiQuestionLine } from "react-icons/ri"
 import { useNavigate } from "react-router-dom"
 import { Toast } from 'primereact/toast'
 import axios from "axios"
-import { useColaboradorContext } from "../../../contexts/Colaborador"
-import { useSessaoUsuarioContext } from "../../../contexts/SessaoUsuario";
- 
+
 const Col12 = styled.div`
     display: flex;
     flex-wrap: wrap;
@@ -42,38 +36,12 @@ const ContainerButton = styled.div`
 `
 
 function AusenciaDadosIniciais() {
-
     const navegar = useNavigate()
-    const [estados, setEstados] = useState([]);
+    const [dataInicialFerias, setDataInicialFerias] = useState('');
+    const [dataFinalFerias, setDataFinalFerias] = useState('');
+    const [classError, setClassError] = useState([]);
+    const [estados, setEstados] = useState([])
     const [loading, setLoading] = useState(false)
-    const [classError, setClassError] = useState([])
-    const navigate = useNavigate();
-
-    const { 
-        usuario,
-        retornarCompanySession
-    } = useSessaoUsuarioContext()
-
-    const { 
-        colaborador,
-        setName,
-        setEmail,
-        setCpf,
-        setDateBirth,
-        setPhoneNumber,
-        setAddressPostalCode,
-        setAddressStreet,
-        setCompanyPublicId,
-        setAdicionarDepartamento,
-        setAddressNumber,
-        setAddressComplement,
-        setAddressDistrict,
-        setAddressCity,
-        setAddressState,
-        setSolicitarCartao,
-        setDepartments,
-        submeterUsuario
-    } = useColaboradorContext()
     
     const toast = useRef(null)
     
@@ -134,23 +102,17 @@ function AusenciaDadosIniciais() {
     }
 
     const adicionarColaborador = () => {
-        if(colaborador.solicitar_cartao)
-        {
-            navegar('/colaborador/registro/envio-cartao')
-        }
-        else
-        {
-            setLoading(true)
-            submeterUsuario().then(response => {
-                if(response.success)
-                {
-                    navegar('/colaborador/registro/sucesso')
-                }
-            }).catch((erro) => {
-                toast.current.show({ severity: 'error', summary: 'Erro', detail: erro.message })
-                setLoading(false)
-            })
-        }       
+      
+        setLoading(true)
+        submeterUsuario().then(response => {
+            if(response.success)
+            {
+                navegar('/colaborador/registro/sucesso')
+            }
+        }).catch((erro) => {
+            toast.current.show({ severity: 'error', summary: 'Erro', detail: erro.message })
+            setLoading(false)
+        })      
     }
 
     const ChangeCep = (value) => 
@@ -178,152 +140,41 @@ function AusenciaDadosIniciais() {
             <Loading opened={loading} />
             <Frame estilo="spaced">
                 <Titulo>
-                    <h6>Dados do Colaborador</h6>
+                    <h6>Solicitação de Férias</h6>
                 </Titulo>
             </Frame>
-            <Col12 >
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        patternMask={['999.999.999-99']} 
-                        name="cpf" 
-                        valor={colaborador.cpf} 
-                        setValor={setCpf} 
-                        type="text" 
-                        label="CPF" 
-                        placeholder="Digite o CPF do colaborador" />
-                </Col6>
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        name="name" 
-                        valor={colaborador.name} 
-                        setValor={setName} 
-                        type="text" 
-                        label="Nome do colaborador" 
-                        placeholder="Digite o name completo do colaborador" />
-                </Col6>
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        name="email" 
-                        valor={colaborador.email} 
-                        setValor={setEmail} 
-                        type="email" 
-                        label="Email do colaborador" 
-                        placeholder="Digite o email do colaborador" />
-                </Col6>
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        patternMask={['99 9999-9999', '99 99999-9999']} 
-                        name="phone_number" 
-                        valor={colaborador.phone_number} 
-                        setValor={setPhoneNumber} 
-                        type="text" 
-                        label="Celular do colaborador" 
-                        placeholder="Digite o telefone do colaborador" />
-                </Col6>
-            </Col12>
-            <Col12>
-                <Col6>
-                    <CheckboxContainer name="remember" valor={colaborador.adicionar_departamento} setValor={setAdicionarDepartamento} label="Adicionar esse colaborador em um departamento" />
-                </Col6>
-            </Col12>
-            {colaborador.adicionar_departamento &&
-                <>
-                    <Frame estilo="spaced">
-                        <Titulo>
-                            <h6>Departamento</h6>
-                        </Titulo>
-                    </Frame>
-                    <DepartamentosRecentes setValor={setDepartments} />
-                </>
-            }
-            <Frame estilo="spaced">
-                <Titulo>
-                    <h6>Endereço do Colaborador</h6>
-                </Titulo>
-                <QuestionCard element={<small>Porque precisamos do endereço?</small>}>
+            <Frame alinhamento="end">
+                <QuestionCard element={<small>Por que precisamos do período de férias?</small>}>
                     <RiQuestionLine className="question-icon" />
                 </QuestionCard>
             </Frame>
-            <Col12 >
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        patternMask={['99999-999']} 
-                        name="address_postal_code" 
-                        valor={colaborador.address_postal_code} 
-                        setValor={ChangeCep} 
-                        type="text" 
-                        label="CEP" 
-                        placeholder="Digite o CEP do colaborador" />
-                </Col6>
-            </Col12>
             <Col12>
                 <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        name="address_street" 
-                        valor={colaborador.address_street} 
-                        setValor={setAddressStreet} 
-                        type="text" 
-                        label="Logradouro" 
-                        placeholder="Digite o address_street do colaborador" />
+                    <CampoTexto
+                        camposVazios={classError}
+                        name="data_inicial_ferias"
+                        valor={dataInicialFerias}
+                        setValor={setDataInicialFerias}
+                        type="date"
+                        label="Data Inicial de Férias"
+                        placeholder="Selecione a data inicial"
+                    />
                 </Col6>
                 <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        name="address_district" 
-                        valor={colaborador.address_district} 
-                        setValor={setAddressDistrict} 
-                        type="text" 
-                        label="Bairro" 
-                        placeholder="Digite o Bairro do colaborador" />
-                </Col6>
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        name="address_number" 
-                        valor={colaborador.address_number} 
-                        setValor={setAddressNumber} 
-                        type="text" 
-                        label="Número" 
-                        placeholder="Digite o número do colaborador" />
-                </Col6>
-                <Col6>
-                    <CampoTexto 
-                        name="address_complement" 
-                        valor={colaborador.address_complement} 
-                        setValor={setAddressComplement} 
-                        type="text" 
-                        label="Complemento (opcional)" 
-                        placeholder="Digite o address_complement do colaborador" />
-                </Col6>
-                <Col6>
-                    <CampoTexto 
-                        camposVazios={classError} 
-                        name="address_city" 
-                        valor={colaborador.address_city} 
-                        setValor={setAddressCity} 
-                        type="text" 
-                        label="Cidade" 
-                        placeholder="Digite a address_city do colaborador" />
-                </Col6>
-                <Col6>
-                    <DropdownItens camposVazios={classError} valor={colaborador.address_state} setValor={setAddressState} options={estados} label="UF" name="address_state" placeholder="Digite a UF do colaborador"/>
-                </Col6>
-                <Col6>
-                    <div className={styles.ladoALado}>
-                        <Texto weight={700}>Solicitar cartão para o colaborador</Texto>
-                        <SwitchInput checked={colaborador.solicitar_cartao} onChange={setSolicitarCartao} />
-                    </div>
+                    <CampoTexto
+                        camposVazios={classError}
+                        name="data_final_ferias"
+                        valor={dataFinalFerias}
+                        setValor={setDataFinalFerias}
+                        type="date"
+                        label="Data Final de Férias"
+                        placeholder="Selecione a data final"
+                    />
                 </Col6>
             </Col12>
             <ContainerButton>
-                <Botao aoClicar={() => navigate('/colaborador')} estilo="neutro" formMethod="dialog" size="medium" filled>Cancelar</Botao>
-                <Botao aoClicar={(evento) => sendData(evento)} estilo="vermilion" size="medium" filled>Adicionar Colaborador</Botao>
+                <Botao aoClicar={() => navigate('/ferias')} estilo="neutro" formMethod="dialog" size="medium" filled>Cancelar</Botao>
+                <Botao aoClicar={(evento) => sendData(evento)} estilo="vermilion" size="medium" filled>Adicionar Férias</Botao>
             </ContainerButton>
         </form>
     )
