c3e261dd9aa8955f2d97cf96f3dee29030aa7339
ariany-f <ariany_f@hotmail.com>
2025-02-13 13:53:56 -0300

    Contrato fornecedor

--- src/pages/Contratos/lista.jsx
+++ src/pages/Contratos/lista.jsx
@@ -2,6 +2,7 @@ import Botao from '@components/Botao'
 import BotaoGrupo from '@components/BotaoGrupo'
 import Titulo from '@components/Titulo'
 import BotaoVoltar from '@components/BotaoVoltar'
+import styles from './Contratos.module.css'
 import { GrAddCircle } from 'react-icons/gr'
 import styled from "styled-components"
 import { Link, Outlet, useLocation, useOutlet, useOutletContext } from "react-router-dom"
@@ -10,6 +11,7 @@ import { useState, useEffect } from 'react'
 import React, { createContext, useContext } from 'react';
 import { useVagasContext } from '@contexts/VagasContext'; // Importando o contexto
 import DataTableContratos from '../../components/DataTableContratos'
+import ModalContratos from '../../components/ModalContratos'
 
 const ConteudoFrame = styled.div`
     display: flex;
@@ -21,11 +23,21 @@ const ConteudoFrame = styled.div`
 const ContratosLista = () => {
 
     const location = useLocation();
+    const [modalOpened, setModalOpened] = useState(false)
     const context = useOutletContext()
 
     return (
         <ConteudoFrame>
+            
+            <BotaoGrupo align="end">
+                <BotaoGrupo align="center">
+                    <Botao aoClicar={() => setModalOpened(true)} estilo="vermilion" size="small" tab><GrAddCircle className={styles.icon} fill="white" color="white"/> Criar Contrato</Botao>
+                </BotaoGrupo>
+            </BotaoGrupo>
+
             <DataTableContratos contratos={context} />
+
+            <ModalContratos opened={modalOpened} aoFechar={() => setModalOpened(false)} />
         </ConteudoFrame>
     );
 };
