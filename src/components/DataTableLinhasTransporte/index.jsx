178108d99c6d39108123ea774fff14d9969d605a
ariany-f <ariany_f@hotmail.com>
2025-02-20 07:53:29 -0300

    Filtro linhas de transporte

--- src/components/DataTableLinhasTransporte/index.jsx
+++ src/components/DataTableLinhasTransporte/index.jsx
@@ -4,6 +4,9 @@ import { Column } from 'primereact/column';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 import './DataTable.css'
 import CampoTexto from '@components/CampoTexto';
+import { InputText } from 'primereact/inputtext';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
@@ -18,10 +21,15 @@ function DataTableLinhasTransporte({ linhas }) {
     const [globalFilterValue, setGlobalFilterValue] = useState('');
     const [filters, setFilters] = useState({
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        fornecedor: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        codigo: { value: null, matchMode: FilterMatchMode.EQUALS },
+        nome: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        operadora: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
     })
     const navegar = useNavigate()
 
-    const onGlobalFilterChange = (value) => {
+    const onGlobalFilterChange = (e) => {
+        const value = e.target.value;
         let _filters = { ...filters };
 
         _filters['global'].value = value;
@@ -42,6 +50,14 @@ function DataTableLinhasTransporte({ linhas }) {
         )
     }
 
+    const renderHeader = () => {
+        return (
+            <></>
+        );
+    };
+
+    const header = renderHeader();
+
     return (
         <>
             <div className="flex justify-content-end">
@@ -49,11 +65,11 @@ function DataTableLinhasTransporte({ linhas }) {
                     <CampoTexto  width={'320px'} valor={globalFilterValue} setValor={onGlobalFilterChange} type="search" label="" placeholder="Buscar premiação" />
                 </span>
             </div>
-            <DataTable value={linhas} filters={filters} globalFilterFields={['nome', 'operadora', 'codigo']}  emptyMessage="Não foram encontrados registros" selection={selectedPremiacao} onSelectionChange={(e) => verDetalhes(e.value)} selectionMode="single" paginator rows={7}  tableStyle={{ minWidth: '68vw' }}>
-                <Column field="codigo" header="Codigo" style={{ width: '15%' }}></Column>
-                <Column field="fornecedor" header="Fornecedor" style={{ width: '20%' }}></Column>
-                <Column field="operadora" header="Operadora" style={{ width: '25%' }}></Column>
-                <Column field="nome" header="Nome" style={{ width: '30%' }}></Column>
+            <DataTable header={header} value={linhas} filters={filters} filterDisplay="row" globalFilterFields={['nome', 'operadora', 'codigo', 'fornecedor']}  emptyMessage="Não foram encontrados registros" selection={selectedPremiacao} onSelectionChange={(e) => verDetalhes(e.value)} selectionMode="single" paginator rows={7}  tableStyle={{ minWidth: '68vw' }}>
+                <Column field="codigo" header="Codigo" style={{ width: '15%' }} filter filterPlaceholder="Filtrar"></Column>
+                <Column field="fornecedor" header="Fornecedor" style={{ width: '20%' }} filter filterPlaceholder="Filtrar"></Column>
+                <Column field="operadora" header="Operadora" style={{ width: '25%' }} filter filterPlaceholder="Filtrar"></Column>
+                <Column field="nome" header="Nome" style={{ width: '30%' }} filter filterPlaceholder="Filtrar"></Column>
                 <Column body={representativeTarifaTemplate} field="tarifa" header="Tarifa" style={{ width: '20%' }}></Column>
                 
             </DataTable>
