5361f26839e48fb58d659e6ad8ae831ef220599a
ariany-f <ariany_f@hotmail.com>
2025-02-23 21:40:35 -0300

    ícone beneficio

--- src/components/DataTableElegibilidadeDetalhes/index.jsx
+++ src/components/DataTableElegibilidadeDetalhes/index.jsx
@@ -1,12 +1,16 @@
 import { DataTable } from 'primereact/datatable';
 import { FilterMatchMode, FilterOperator } from 'primereact/api';
 import { Column } from 'primereact/column';
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { MdOutlineFastfood, MdOutlineKeyboardArrowRight, MdOutlineMedicalServices } from 'react-icons/md'
 import './DataTable.css'
 import BadgeGeral from '@components/BadgeGeral';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { RiBusFill, RiComputerLine, RiGasStationFill, RiShoppingCartFill } from 'react-icons/ri';
+import { PiForkKnifeFill } from 'react-icons/pi';
+import { BiBookReader } from 'react-icons/bi';
+import { FaTheaterMasks } from 'react-icons/fa';
 
 let Real = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
@@ -28,6 +32,90 @@ const Col6 = styled.div`
     gap: 8px;
     flex-direction: column;
 `
+
+const icones = [
+    {
+        "id": 1,
+        "name": "Alimentação",
+        "flexible_value": false,
+        "description": "Mercados, supermercados e aplicativo de delivery.",
+        "food_meal_one_category": false,
+        "icone": <RiShoppingCartFill size={20} />
+    },
+    {
+        "id": 2,
+        "name": "Refeição",
+        "flexible_value": false,
+        "description": "Restaurantes, cafeterias, padarias, mercados, aplicativo de delivery e lojas de conveniência.",
+        "food_meal_one_category": false,
+        "icone": <MdOutlineFastfood size={20} />
+    },
+    {
+        "id": 3,
+        "name": "Mobilidade",
+        "flexible_value": true,
+        "description": "Postos de combustível, estacionamentos, pedágio, carros por aplicativo, recarga de bilhete de transporte e passagens de ônibus e trem.",
+        "food_meal_one_category": false,
+        "icone": <RiBusFill size={20} />
+    },
+    {
+        "id": 4,
+        "name": "Home Office",
+        "flexible_value": true,
+        "description": "Compra de cadeira ergométrica, itens de papelaria, assistência técnica de computador e custeio de contas de energia e internet",
+        "food_meal_one_category": false,
+        "icone": <RiComputerLine size={20} />
+    },
+    {
+        "id": 5,
+        "name": "Combustível",
+        "flexible_value": true,
+        "description": "",
+        "food_meal_one_category": false,
+        "icone": <RiGasStationFill size={20} />
+    },
+    {
+        "id": 6,
+        "name": "Cultura",
+        "flexible_value": true,
+        "description": "Streaming de vídeo e música, bancas de jornais, jogos online, ingressos para shows teatros e museus, instrumentos musicais, escolas de arte e música e parques de diversões, zoológicos e aquários.",
+        "food_meal_one_category": false,
+        "icone": <FaTheaterMasks size={20} />
+    },
+    {
+        "id": 7,
+        "name": "Educação",
+        "flexible_value": true,
+        "description": "Cursos online e presenciais, cursos de extensão, cursos e app de idiomas, ensino superior e técnico, eventos e feiras profissionais e livrarias e papelarias",
+        "food_meal_one_category": false,
+        "icone": <BiBookReader  size={20} />
+    },
+    {
+        "id": 8,
+        "name": "Saúde",
+        "flexible_value": true,
+        "description": "Farmácias, exames, consultas, serviços hospitalares, serviços médicos eterapias.",
+        "food_meal_one_category": false,
+        "icone": <MdOutlineMedicalServices size={20} />
+    },
+    {
+        "id": 9,
+        "name": "Auxílio Alimentação",
+        "flexible_value": false,
+        "description": "Alimentação e Refeição, tudo em uma só categoria.",
+        "food_meal_one_category": true,
+        "icone": <PiForkKnifeFill size={20} />
+    },
+    {
+        "id": 10,
+        "name": "Vale Combustivel",
+        "flexible_value": true,
+        "description": "",
+        "food_meal_one_category": false,
+        "icone": <RiGasStationFill size={20} />
+    }
+]
+
 
 function DataTableElegibilidadeDetalhes({ elegibilidade, pagination = true }) {
 
@@ -53,7 +141,7 @@ function DataTableElegibilidadeDetalhes({ elegibilidade, pagination = true }) {
             Real.format(rowData.valor)
         )
     }
-    
+
     const representativeBeneficiosTemplate = (rowData) => {
         return (
             <>
@@ -62,12 +150,22 @@ function DataTableElegibilidadeDetalhes({ elegibilidade, pagination = true }) {
                 {rowData.beneficios.map((benefit, index) => {
                     return (
                         <Col6 key={index}>
-                            <BadgeGeral  nomeBeneficio={
-                                    <>
-                                        {benefit.beneficio} <br /> 
-                                        {representativeValorTemplate(benefit)}
-                                    </>
-                                }  />
+                            {icones.map(item => {
+                                if(item.name == benefit.beneficio)
+                                {
+                                    return (
+                                        <BadgeGeral  nomeBeneficio={
+                                            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                                                <div>{item.icone}</div>
+                                                <div>
+                                                {benefit.beneficio} <br /> 
+                                                {representativeValorTemplate(benefit)}
+                                                </div>
+                                            </div>
+                                        }  />
+                                    )
+                                }
+                            })}    
                         </Col6>
                     )
                 })}
