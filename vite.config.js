406108df29c93fe68b163dcd0c50e4b67c84174e
ariany-f <ariany_f@hotmail.com>
2025-03-03 13:21:27 -0300

    Removendo api

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://apibeneficios.aqbank.com.br',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, './src') },
