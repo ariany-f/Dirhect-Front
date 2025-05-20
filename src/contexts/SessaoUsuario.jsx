15bcb67b9d99778a2e84831db901ca5e2da8562f
ariany-f <ariany_f@hotmail.com>
2025-05-20 12:29:29 -0300

    Ajuste usuario esta logado

--- src/contexts/SessaoUsuario.jsx
+++ src/contexts/SessaoUsuario.jsx
@@ -115,7 +115,7 @@ export const SessaoUsuarioProvider = ({ children }) => {
     });
     const [recuperacaoSenha, setRecuperacaoSenha] = useState(recuperacaoSenhaInicial)
     // Retornar para validar se existe o access token no Armazeanador Token ao invés do CPF na seção
-    const [usuarioEstaLogado, setUsuarioEstaLogado] = useState(!!usuario.cpf)
+    const [usuarioEstaLogado, setUsuarioEstaLogado] = useState(!!usuario.email)
 
     const setRecuperacaoToken = (token) => {
         setRecuperacaoSenha(estadoAnterior => {
