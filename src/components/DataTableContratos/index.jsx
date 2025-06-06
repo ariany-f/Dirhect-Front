269353926a21d45350e3ef9c457a7e40eedc0b51
ariany-f <ariany_f@hotmail.com>
2025-06-06 15:42:28 -0300

    Ajuste

--- src/components/DataTableContratos/index.jsx
+++ src/components/DataTableContratos/index.jsx
@@ -471,7 +471,7 @@ function DataTableContratos({
                 {/* <Column field="num_contrato_origem" sortField="num_contrato_origem" sortable header="Número Contrato" style={{ width: '10%' }}></Column> */}
                 <Column body={representativeInicioTemplate} field="dt_inicio" header="Data Início" style={{ width: '10%' }}></Column>
                 <Column body={representativeFimTemplate} field="dt_fim" header="Data Fim" style={{ width: '10%' }}></Column>
-                <Column body={representativSituacaoTemplate} header="Situação" style={{ width: '12%' }}></Column>
+                <Column body={representativSituacaoTemplate} header="Situação" style={{ width: '10%' }}></Column>
                 <Column body={representativeBeneficioRegraElegibilidadeTemplate} header="Benefícios com Regras" style={{ width: '10%', textAlign: 'center' }}></Column>
                 <Column body={representativeRegraElegibilidadeTemplate} header="Itens com Regras" style={{ width: '10%', textAlign: 'center' }}></Column>
                 <Column body={representativeActionsTemplate} header="" style={{ width: '15%', textAlign: 'center' }}></Column>
