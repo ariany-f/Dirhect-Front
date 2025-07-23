be9edd8c8c1eb2ebd747b4bfa82d8f0a23251d8f
ariany-f <ariany_f@hotmail.com>
2025-07-22 21:38:16 -0300

    Adiciona integração com Sentry para monitoramento de erros em produção e atualiza dependência do Sentry no package.json.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './routes.jsx'
 import 'primeicons/primeicons.css';
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppRouter />
-   </React.StrictMode>
-)
+const app = (
+  <AppRouter />
+);
+
+if (import.meta.env.VITE_MODE === 'production') {
+  Sentry.init({
+    dsn: "https://87f4b21d4a2b4070937882709e03efa0@o4509715042402304.ingest.us.sentry.io/4509715044958208",
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true
+  });
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(import.meta.env.VITE_MODE === 'development' ? (
+    <React.StrictMode>{app}</React.StrictMode>
+) : (
+    app
+));
